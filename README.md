9cc72995ba419b3742108b852cc241a3cd07c6ed
John GALT <john@crypto7.world>
2024-11-13 17:42:38 +0100

    wip

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # heritage-gui
+
 Managing Bitcoin inheritance on-chain with the Heritage Wallet GUI.
+
+## Development
+
+Run the following command in the root of the project to start the Dioxus dev server:
+
+```bash
+dx serve --hot-reload --platform desktop
+```
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+use dioxus_logger::tracing::{info, Level};
+
+fn main() {
+    // Init logger
+    dioxus_logger::init(Level::INFO).expect("failed to init logger");
+    info!("starting app");
+
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    // Build cool things ✌️
+
+    rsx! {
+        link { rel: "stylesheet", href: "assets/main.css" }
+        img { src: "assets/header.svg", id: "header" }
+        div { id: "links",
+            a { href: "https://dioxuslabs.com/learn/0.5/", "📚 Learn Dioxus" }
+            a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
+            a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
+            a { href: "https://github.com/DioxusLabs/dioxus-std", "⚙️ Dioxus Standard Library" }
+            a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
+                "💫 VSCode Extension"
+            }
+            a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+        }
+    }
+}
