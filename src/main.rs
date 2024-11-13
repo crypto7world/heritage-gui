e9f721d1f824f0a0c7801bdea5eee903bcf98073
John GALT <john@crypto7.world>
2024-11-14 00:00:06 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,130 @@
 #![allow(non_snake_case)]
 
-use dioxus::prelude::*;
+mod components;
+mod views;
+
+use dioxus::{
+    desktop::{Config, WindowBuilder},
+    prelude::*,
+};
 use dioxus_logger::tracing::{info, Level};
+
+#[derive(Clone, Routable, Debug, PartialEq)]
+#[rustfmt::skip]
+enum Route {
+    #[layout(NavBar)]
+        #[route("/")]
+        Home {},
+        #[nest("/blog")]
+            #[layout(Blog)]
+                #[route("/")]
+                BlogList {},
+                #[route("/post/:name")]
+                BlogPost { name: String },
+            #[end_layout]
+        #[end_nest]
+    #[end_layout]
+    #[nest("/myblog")]
+        #[redirect("/", || Route::BlogList {})]
+        #[redirect("/:name", |name: String| Route::BlogPost { name })]
+    #[end_nest]
+    #[route("/:..route")]
+    PageNotFound {
+        route: Vec<String>,
+    },
+}
 
 fn main() {
     // Init logger
     dioxus_logger::init(Level::INFO).expect("failed to init logger");
     info!("starting app");
 
-    dioxus::launch(App);
+    LaunchBuilder::desktop()
+        .with_cfg(
+            Config::new().with_window(
+                WindowBuilder::new()
+                    .with_title("Heritage Wallet")
+                    .with_resizable(true),
+            ),
+        )
+        .launch(App)
+}
+
+pub fn App() -> Element {
+    rsx! {
+        Router::<Route> {}
+    }
 }
 
 #[component]
-fn App() -> Element {
-    // Build cool things ✌️
-
+fn NavBar() -> Element {
     rsx! {
-        link { rel: "stylesheet", href: "assets/main.css" }
-        img { src: "assets/header.svg", id: "header" }
-        div { id: "links",
-            a { href: "https://dioxuslabs.com/learn/0.5/", "📚 Learn Dioxus" }
-            a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-            a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-            a { href: "https://github.com/DioxusLabs/dioxus-std", "⚙️ Dioxus Standard Library" }
-            a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
-                "💫 VSCode Extension"
+        nav {
+            ul {
+                li {
+                    Link { to: Route::Home {}, "Home" }
+                }
+                li {
+                    Link { to: Route::BlogList {}, "Blog" }
+                }
             }
-            a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
         }
+        Outlet::<Route> {}
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    rsx! {
+        h1 { "Welcome to the Dioxus Blog!" }
+    }
+}
+
+#[component]
+fn Blog() -> Element {
+    rsx! {
+        h1 { "Blog" }
+        Outlet::<Route> {}
+    }
+}
+
+#[component]
+fn BlogList() -> Element {
+    rsx! {
+        h2 { "Choose a post" }
+        ul {
+            li {
+                Link {
+                    to: Route::BlogPost {
+                        name: "Blog post 1".into(),
+                    },
+                    "Read the first blog post"
+                }
+            }
+            li {
+                Link {
+                    to: Route::BlogPost {
+                        name: "Blog post 2".into(),
+                    },
+                    "Read the second blog post"
+                }
+            }
+        }
+    }
+}
+
+#[component]
+fn BlogPost(name: String) -> Element {
+    rsx! {
+        h2 { "Blog Post: {name}" }
+    }
+}
+
+#[component]
+fn PageNotFound(route: Vec<String>) -> Element {
+    rsx! {
+        h1 { "Page not found" }
+        p { "We are terribly sorry, but the page you requested doesn't exist." }
+        pre { color: "red", "log:\nattemped to navigate to: {route:?}" }
     }
 }
